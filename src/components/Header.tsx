8b368af8d1fa149e90fa95d39e7cbc4a007b80e9
silvershade1 <silvershade1000@gmail.com>
2024-08-15 12:55:39 +0330

    refactor : refactor header code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,19 +15,17 @@ function Header() {
 
   return (
     <header className="flex items-center justify-between p-4 md:p-6 bg-white shadow-md relative">
-      {/* Logo Section */}
       <div className="flex items-center">
         <Link to="/" className="mr-4">
           <img src={logo} alt="Logo" className="h-6 md:h-8 lg:h-10" />
         </Link>
       </div>
 
-      {/* Search Bar (Hidden on Small Screens) */}
+
       <div className="hidden md:flex flex-1 mx-2 md:mx-4 lg:mx-8">
         <SearchBar />
       </div>
 
-      {/* Mobile Menu Button */}
       <div className="md:hidden ml-auto">
         <button
           onClick={toggleMenu}
@@ -37,7 +35,7 @@ function Header() {
         </button>
       </div>
 
-      {/* Navigation Menu (Hidden on Small Screens) */}
+
       <ul className="hidden md:flex items-center space-x-4 lg:space-x-6">
         <li className="p-2 lg:p-3 hover:text-orange text-gray-600 transition-all duration-300 ease-in">
           <Link to="/pricing">Plans & Pricing</Link>
@@ -50,14 +48,14 @@ function Header() {
         </li>
       </ul>
 
-      {/* Shopping Cart & Auth Links (Hidden on Small Screens) */}
+
       <div className="hidden md:flex items-center space-x-4">
         <RiShoppingCart2Line className="text-gray-600" size={28} />
         <AuthLink text="Login" type="login" href="/login" />
         <AuthLink text="Sign Up" type="signup" href="/signup" />
       </div>
 
-      {/* Mobile Dropdown Menu (Opens from the Left) */}
+
       {isMenuOpen && (
         <ul className="fixed top-0 left-0 h-full w-64 bg-white p-4 space-y-4 shadow-lg md:hidden z-50">
           <li className="flex justify-end">
