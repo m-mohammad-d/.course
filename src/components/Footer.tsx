4051abd7c6b59de5efa470a8eab55425b8642ddb
silvershade1 <silvershade1000@gmail.com>
2024-08-15 11:54:22 +0330

    feat: make footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ import logo from "../asset/image/udemyLogo.png";
 
 function Footer() {
   return (
-    <footer className="bg-[#2D2F31] text-white py-8 mt-auto ">
+    <footer className="bg-[#2D2F31] text-white py-8 mt-auto">
       <div className="container mx-auto px-4">
-        <div className="flex justify-between">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-16 text-[12px] mb-8">
+        <div className="flex flex-col md:flex-row justify-between mb-8">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 text-[12px] mb-8 md:mb-0">
             <div>
               <ul className="space-y-3">
                 <li className="transition-all delay-300 ease-linear hover:text-orangePeel hover:underline">
@@ -65,16 +65,16 @@ function Footer() {
               </ul>
             </div>
           </div>
-          <div className="text-center md:text-left">
-            <button className="border border-white text-white py-2 px-4 rounded mb-4 md:mb-0 flex items-center justify-center">
-              <IoEarthOutline />
+          <div className="text-center md:text-left mb-8 md:mb-0">
+            <button className="border border-white text-white py-2 px-4 rounded flex items-center justify-center">
+              <IoEarthOutline className="mr-2" />
               English
             </button>
           </div>
         </div>
-        <div className="flex justify-between items-center">
-          <div>
-            <img src={logo} alt="Udemy Logo" className="h-8" />
+        <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left">
+          <div className="mb-4 md:mb-0">
+            <img src={logo} alt="Udemy Logo" className="h-8 mx-auto md:mx-0" />
           </div>
           <p className="text-xs">&copy; 2024 Udemy, Inc.</p>
         </div>
