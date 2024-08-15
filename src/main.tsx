d804543f3d1fd249a99a6323e4d427bdc1c22b46
silvershade1 <silvershade1000@gmail.com>
2024-08-15 18:35:23 +0330

    refactor : refacotr main code

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import App from "./App";
 import "./index.css";
 
 const queryClient = new QueryClient();
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Toaster />
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
 
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
+  );
+}
