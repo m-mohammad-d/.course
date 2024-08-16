618d676db3f0769df3be0d32fc937990e6ddd761
silvershade1 <silvershade1000@gmail.com>
2024-08-16 13:22:24 +0330

    Refactor: TeacherLayout to pass Sidebar items as props

--- src/layout/TeacherLayout.tsx
+++ src/layout/TeacherLayout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/SideBar";
-
+const items = [
+  { name: "editProfile", path: "/tehacer/edit-profile" },
+  { name: "Password", path: "/teacher/edit-password" },
+  { name: "Photo", path: "#" },
+  { name: "myCourse", path: "/teacher/myCourse" },
+  { name: "addNewCourse", path: "/user/addNewCourse" },
+  { name: "Privacy", path: "#" },
+  { name: "Notifications", path: "#" },
+  { name: "API clients", path: "#" },
+];
 function TeacherLayout() {
   return (
     <div className="flex justify-center my-12 px-4">
       <div className="flex flex-col md:flex-row w-full max-w-7xl">
         <div className="w-full md:w-1/4 mb-4 md:mb-0">
-          <Sidebar />
+          <Sidebar items={items} />
         </div>
         <div className="w-full md:w-3/4 md:ml-8">
           <Outlet />
