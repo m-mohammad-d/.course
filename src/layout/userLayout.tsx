b2575ec67fe25fc48fc25f0d8dfb7c3f31ea0e17
silvershade1 <silvershade1000@gmail.com>
2024-08-16 13:19:29 +0330

    Refactor: UserLayout to pass Sidebar items as props

--- src/layout/userLayout.tsx
+++ src/layout/userLayout.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/SideBar";
-
+const items = [
+  { name: "edit profile", path: "/user/edit-profile" },
+  { name: "Password", path: "/user/edit-password" },
+  { name: "Photo", path: "/user/photo" },
+  { name: "Subscriptions", path: "/user/subscriptions" },
+  { name: "Payment methods", path: "/user/payment-methods" },
+  { name: "Privacy", path: "/user/privacy" },
+  { name: "Notifications", path: "/user/notifications" },
+  { name: "API clients", path: "/user/api-clients" },
+];
 function UserLayout() {
   return (
     <div className="flex justify-center my-12 px-4">
       <div className="flex flex-col md:flex-row w-full max-w-7xl">
         <div className="w-full md:w-1/4 mb-4 md:mb-0">
-          <Sidebar />
+          <Sidebar items={items} />
         </div>
         <div className="w-full md:w-3/4 md:ml-8">
           <Outlet />
