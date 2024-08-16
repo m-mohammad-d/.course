5bbb34f896495fdcec25a43e5ee0c1adfdf5d732
silvershade1 <silvershade1000@gmail.com>
2024-08-16 13:15:27 +0330

    refactor : refactor sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,23 +6,17 @@ interface SidebarItem {
   path: string;
 }
 
-const items: SidebarItem[] = [
-  { name: "edit profile", path: "/user/edit-profile" },
-  { name: "Password", path: "/user/edit-password" },
-  { name: "Photo", path: "/user/photo" },
-  { name: "Subscriptions", path: "/user/subscriptions" },
-  { name: "Payment methods", path: "/user/payment-methods" },
-  { name: "Privacy", path: "/user/privacy" },
-  { name: "Notifications", path: "/user/notifications" },
-  { name: "API clients", path: "/user/api-clients" },
-];
-function Sidebar() {
+interface SidebarProps {
+  items: SidebarItem[];
+}
+
+function Sidebar({ items }: SidebarProps) {
   const { logout } = useLogout();
   function handlerLogout() {
     logout();
   }
   return (
-    <div className="w-full md:w-64 bg-gray-100 p-4 border-gray-300 border">
+    <div className="w-full md:w-64 bg-gray-100 p-4 border-gray-300 border rounded-lg shadow-lg">
       <div className="flex items-center mb-6">
         <div className="w-16 h-16 bg-gray-500 rounded-full flex items-center justify-center text-white text-2xl">
           M
@@ -36,17 +30,17 @@ function Sidebar() {
           <li key={index} className="mb-2">
             <Link
               to={item.path}
-              className="block p-2 text-gray-800 rounded hover:bg-gray-200"
+              className="block p-2 text-gray-800 rounded hover:bg-gray-200 transition duration-200"
             >
               {item.name}
             </Link>
           </li>
         ))}
         <button
-          className="block p-2 text-gray-800 rounded hover:bg-gray-200"
+          className="block p-2 text-gray-800 rounded hover:bg-gray-200 transition duration-200"
           onClick={handlerLogout}
         >
-          log out
+          Log Out
         </button>
       </ul>
     </div>
