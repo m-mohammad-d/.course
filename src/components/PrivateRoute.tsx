5250a8bd6f41ec49a78df9c5111dc51f8ac9cacc
silvershade1 <silvershade1000@gmail.com>
2024-08-16 14:08:13 +0330

    feat : add admin role  to praivate route

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,3 +1,4 @@
+
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { getUserRole } from "../utils/getUserRole";
 
@@ -7,16 +8,21 @@ const PrivateRoute = () => {
 
   const isUserRoute = location.pathname.startsWith("/user");
   const isTeacherRoute = location.pathname.startsWith("/teacher");
+  const isAdminRoute = location.pathname.startsWith("/admin");
 
   if (isUserRoute && userRole === "user") {
     return <Outlet />;
   } else if (isTeacherRoute && userRole === "teacher") {
     return <Outlet />;
-  } else if (!isUserRoute && !isTeacherRoute && userRole) {
+  } else if (isAdminRoute && userRole === "admin") {
+    return <Outlet />;
+  } else if (!isUserRoute && !isTeacherRoute && !isAdminRoute && userRole) {
     if (userRole === "user") {
       return <Navigate to="/user/mycourse" />;
     } else if (userRole === "teacher") {
       return <Navigate to="/teacher/dashboard" />;
+    } else if (userRole === "admin") {
+      return <Navigate to="/admin/dashboard" />;
     }
   }
 
