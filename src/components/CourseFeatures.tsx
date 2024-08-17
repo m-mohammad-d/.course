abcdeea14371352d8e07abec336321c6ce177999
silvershade1 <silvershade1000@gmail.com>
2024-08-17 12:32:01 +0330

    style : update CourseFeatures style

--- src/components/CourseFeatures.tsx
+++ src/components/CourseFeatures.tsx
@@ -5,34 +5,36 @@ import { RiFolderDownloadLine } from "react-icons/ri";
 
 function CourseFeatures({ courseTime }: { courseTime: string }) {
   return (
-    <div className=" text-gray2 mt-8 py-6">
-        <h2 className="text-4xl">This course includes: </h2>
-        <ul className="grid grid-cols-2 mt-8 justify-between w-full gap-y-5 text-lg">
-          <li className="flex gap-2 items-center">
-            <FaRegClock />
-            <span>{courseTime} hours on-demand videos</span>
-          </li>
-          <li className="flex gap-2 items-center">
-            <IoPhonePortraitSharp />
-            <span>Access on mobile and TV</span>
-          </li>
-          <li className="flex gap-2 items-center">
-            <FaRegFile />
-            <span>0 articles</span>
-          </li>
-          <li className="flex gap-2 items-center">
-            <FaInfinity />
-            <span>Full lifetime access</span>
-          </li>
-          <li className="flex gap-2 items-center">
-            <RiFolderDownloadLine />
-            <span>0 downloadable resources</span>
-          </li>
-          <li className="flex gap-2 items-center">
-            <LuTrophy />
-            <span>Certificate of completion</span>
-          </li>
-        </ul>
+    <div className="text-gray2 mt-8 py-6">
+      <h2 className="text-4xl font-bold text-darkGray">
+        This course includes:
+      </h2>
+      <ul className="grid grid-cols-1 text-mediumGray md:grid-cols-2 mt-8 gap-y-5 text-lg">
+        <li className="flex gap-2 items-center">
+          <FaRegClock />
+          <span>{courseTime} hours on-demand videos</span>
+        </li>
+        <li className="flex gap-2 items-center">
+          <IoPhonePortraitSharp />
+          <span>Access on mobile and TV</span>
+        </li>
+        <li className="flex gap-2 items-center">
+          <FaRegFile />
+          <span>0 articles</span>
+        </li>
+        <li className="flex gap-2 items-center">
+          <FaInfinity />
+          <span>Full lifetime access</span>
+        </li>
+        <li className="flex gap-2 items-center">
+          <RiFolderDownloadLine />
+          <span>0 downloadable resources</span>
+        </li>
+        <li className="flex gap-2 items-center">
+          <LuTrophy />
+          <span>Certificate of completion</span>
+        </li>
+      </ul>
     </div>
   );
 }
