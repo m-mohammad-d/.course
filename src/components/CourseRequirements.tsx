f3b58042ceaecbbdd28d1797e9a3eb9123f525a8
silvershade1 <silvershade1000@gmail.com>
2024-08-17 12:46:07 +0330

    style : update CourseRequirements Style

--- src/components/CourseRequirements.tsx
+++ src/components/CourseRequirements.tsx
@@ -1,6 +1,6 @@
 function CourseRequirements({ requirements }: { requirements: string }) {
   return (
-    <div className="my-8 space-y-6">
+    <div className="my-8 space-y-6 text-darkGray">
       <h2 className="text-2xl">Requirements</h2>
       <p>{requirements || "This course has no requirements"}</p>
     </div>
