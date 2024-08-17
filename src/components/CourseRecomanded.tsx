93ba0b7b631a055abe572b4994b0732feca0fefd
silvershade1 <silvershade1000@gmail.com>
2024-08-17 21:07:36 +0330

    feat : add data to courserecommended

--- src/components/CourseRecomanded.tsx
+++ src/components/CourseRecomanded.tsx
@@ -1,58 +1,15 @@
 import CourseCards from "./CourseCards";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
-
-const recommendedCourses = [
-  {
-    title: "The Complete Python Bootcamp From Zero to Hero in…",
-    instructor: "Jose Portilla, Pierian Training",
-    rating: 4.6,
-    reviews: 513506,
-    price: "€14.99",
-    oldPrice: "€84.99",
-    image: "https://img-b.udemycdn.com/course/750x422/2263832_ac7c_2.jpg",
-  },
-  {
-    title: "The Ultimate React Course 2024: React, Next.js, Redux & More",
-    instructor: "jonas",
-    rating: 4.9,
-    reviews: 56181,
-    price: "€19.99",
-    oldPrice: "€129.99",
-    image: "https://img-b.udemycdn.com/course/750x422/4471614_361e_8.jpg",
-  },
-  {
-    title: "100 Days of Code: The Complete Python Pro Bootcamp",
-    instructor: "Dr. Angela Yu, Developer and Lead Instructor",
-    rating: 4.7,
-    reviews: 313806,
-    price: "€14.99",
-    oldPrice: "€104.99",
-    image: "https://img-b.udemycdn.com/course/750x422/2776760_f176_10.jpg",
-  },
-  {
-    title: "C++ Programming Bootcamp",
-    instructor: "Ardit Sulce",
-    rating: 4.6,
-    reviews: 67303,
-    price: "€14.99",
-    oldPrice: "€99.99",
-    image: "https://img-b.udemycdn.com/course/750x422/5187422_18d7_7.jpg",
-  },
-  {
-    title: "C# Basics for Beginners: Learn C# Fundamentals by Coding",
-    instructor: "mosh hamedani",
-    rating: 4.6,
-    reviews: 113655,
-    price: "€14.99",
-    oldPrice: "€84.99",
-    image: "https://img-c.udemycdn.com/course/750x422/382002_5d4a_3.jpg",
-  },
-];
+import useGetCourses from "../hooks/useGetCourses";
+import Spinner from "./Spinner";
 
 const CourseRecommendations = () => {
+  const { data, isLoading } = useGetCourses();
+  const recommendedCourses = data?.splice(1, 5);
+  if (isLoading) return <Spinner />;
   return (
-    <div className="max-w-7xl mx-auto py-12">
+    <div className="max-w-7xl mx-auto py-12 h-full">
       <h2 className="text-2xl font-bold mb-6 text-darkGray">
         What to learn next
       </h2>
