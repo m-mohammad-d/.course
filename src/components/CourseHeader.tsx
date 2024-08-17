c828e6bcaf021eaadd6ec9c5baa169d9288d766c
silvershade1 <silvershade1000@gmail.com>
2024-08-17 12:28:39 +0330

    style : update CourseHeader style

--- src/components/CourseHeader.tsx
+++ src/components/CourseHeader.tsx
@@ -23,19 +23,19 @@ function CourseHeader({
   img,
 }: CourseHeaderType) {
   return (
-    <div className="bg-[#2D2F31] text-white py-4">
+    <div className="bg-darkGray text-white py-4">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="flex-1 md:mr-10 mb-8 md:mb-0">
           <h2 className="text-4xl font-bold mb-4">{courseName}</h2>
           <p className="text-lg mb-4">{coursetitle}</p>
           <div className="flex flex-wrap gap-4 items-center mb-6">
-            <div className="px-4 py-2 bg-orange rounded-sm text-sm font-semibold">
+            <div className="px-4 py-2 bg-orangePeel rounded-sm text-sm font-semibold">
               Best Seller
             </div>
             <div className="flex items-center space-x-4">
               <div className="flex gap-1 items-center">
-                <IoStarSharp color="yellow" />
-                <p className=" font-semibold">{courseRating}</p>
+                <IoStarSharp color="#FFD700" />
+                <p className="font-semibold">{courseRating}</p>
               </div>
               <p className="text-sm">({ratingCount} ratings)</p>
             </div>
@@ -45,11 +45,11 @@ function CourseHeader({
             </div>
           </div>
           <p className="text-sm">
-            Created by <span className="text-orange">{instructor}</span>
+            Created by <span className="text-orangePeel">{instructor}</span>
           </p>
         </div>
 
-        <div className=" text-gray3 bg-white p-6 rounded-lg shadow-lg">
+        <div className="text-darkGray bg-white p-6 rounded-lg shadow-lg">
           <img
             src={img}
             alt="Course"
@@ -59,7 +59,7 @@ function CourseHeader({
             <p className="text-2xl font-bold">${price}</p>
             <p className="text-sm text-gray-600">30-Day Money-Back Guarantee</p>
           </div>
-          <button className="w-full bg-orange text-white py-2 rounded-md hover:bg-orange-600 transition duration-300">
+          <button className="w-full bg-orangePeel text-white py-2 rounded-md hover:bg-darkOrange transition duration-300">
             Add to Cart
           </button>
         </div>
