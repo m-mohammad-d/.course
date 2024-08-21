cd365b6e8ac530d06e9dca33ac73dac248057960
silvershade1 <silvershade1000@gmail.com>
2024-08-21 13:47:48 +0330

    fix : fix bug in report page

--- src/pages/FeedbackManagement.tsx
+++ src/pages/FeedbackManagement.tsx
@@ -5,14 +5,14 @@ import useGetFeadBack from "../hooks/useGetFeadBack";
 function FeedbackManagement() {
   const { data, isLoading } = useGetFeadBack();
   if (isLoading) return <Spinner />;
+
   return (
     <div className="bg-backgroundGray p-6 rounded-lg">
-      {data?.map((feedback) => (
-        <FeadBackCard
-          key={feedback.id}
-          feedback={feedback}
-        />
-      ))}
+      {data?.length > 0
+        ? data?.map((feedback) => (
+            <FeadBackCard key={feedback.id} feedback={feedback} />
+          ))
+        : "is not found"}
     </div>
   );
 }
