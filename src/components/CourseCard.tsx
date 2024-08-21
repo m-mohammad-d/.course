350ed5b973fb3b2fd59dfb706fc3ab2cb5faf453
silvershade1 <silvershade1000@gmail.com>
2024-08-21 14:05:13 +0330

    style: improve courseCard  responsiveness for mobile

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -28,28 +28,28 @@ function CourseCard({
   instructors,
 }: CourseCardProps) {
   return (
-    <div className="flex border rounded-lg shadow-xl p-6 bg-softWhite">
-      <div className="flex-shrink-0">
+    <div className="flex flex-col md:flex-row border rounded-lg shadow-xl p-6 bg-softWhite">
+      <div className="flex-shrink-0 mb-4 md:mb-0">
         <img
           src={image}
           alt={title}
-          className="w-40 h-40 object-cover rounded-lg"
+          className="w-full md:w-40 h-auto md:h-40 object-cover rounded-lg"
         />
       </div>
 
-      <div className="flex flex-col justify-between flex-grow ml-4">
+      <div className="flex flex-col justify-between flex-grow md:ml-4">
         <div>
           <Link to={`/course/${id}`}>
-            <h2 className="text-2xl font-bold text-darkGray hover:text-primary transition duration-300 ease-in-out">
+            <h2 className="text-xl md:text-2xl font-bold text-darkGray hover:text-primary transition duration-300 ease-in-out">
               {name}
             </h2>
           </Link>
           <p className="text-textGray mt-2">{title}</p>
-          <p className="text-sm text-mediumGray mt-7">
+          <p className="text-sm text-mediumGray mt-4">
             By {instructors?.name || "Unknown"}
           </p>
         </div>
-        <div className="flex items-center mt-4 text-sm text-mediumGray">
+        <div className="flex flex-wrap items-center mt-4 text-sm text-mediumGray">
           <span className="text-accentYellow font-semibold">{rating} ⭐</span>
           <span className="ml-2">({comments?.length})</span>
           <span className="ml-2">
@@ -58,7 +58,7 @@ function CourseCard({
         </div>
       </div>
 
-      <div className="flex flex-col justify-between items-end text-xl font-bold text-primary">
+      <div className="flex flex-col justify-between items-end text-xl font-bold text-primary mt-4 md:mt-0">
         <span>€{price}</span>
       </div>
     </div>
