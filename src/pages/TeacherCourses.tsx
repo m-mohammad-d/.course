d1f6b8a6373a25473e146a09b400ced4615832e8
silvershade1 <silvershade1000@gmail.com>
2024-08-21 14:26:55 +0330

    feat : add TeacherCourse

--- src/pages/TeacherCourses.tsx
+++ src/pages/TeacherCourses.tsx
@@ -1,5 +1,57 @@
+import CourseCards from "../components/CourseCards";
+import Spinner from "../components/Spinner";
+import useGetCoursesByInstructor from "../hooks/useGetCoursesByInstructor";
+import useGetInstructorByUserid from "../hooks/useGetInstructorByUserId";
+import { getItemLocal } from "../utils/localStorageUtils";
+
+interface AuthToken {
+  user: {
+    id: string;
+  };
+}
+
 function TeacherCourses() {
-  return <div></div>;
+  const userId = getItemLocal<AuthToken>("sb-llgyyyodgevtfoidrwjf-auth-token")
+    ?.user.id;
+
+  const {
+    data: instructor,
+    isLoading: isInstructorLoading,
+    error: isInstructorError,
+  } = useGetInstructorByUserid(userId);
+
+  const {
+    data: courses,
+    error,
+    isLoading,
+  } = useGetCoursesByInstructor(instructor?.id);
+
+  if (isInstructorLoading || isLoading) {
+    return <Spinner />;
+  }
+
+  if (isInstructorError || error) {
+    return <p className="text-gray-600 text-lg">Error loading courses.</p>;
+  }
+
+  return (
+    <div className=" bg-backgroundGray p-8">
+      <div className="bg-primary text-white p-6 rounded-lg shadow-md mb-8">
+        <h1 className="text-3xl font-extrabold text-center">Your Courses</h1>
+      </div>
+      <main>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {courses && courses.length > 0 ? (
+            courses.map((course) => (
+              <CourseCards key={course.id} course={course} />
+            ))
+          ) : (
+            <p className="text-gray-600 text-lg">No courses available.</p>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default TeacherCourses;
