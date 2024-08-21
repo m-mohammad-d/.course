8055620157e886c61f83c2b624f604ab763e26ac
silvershade1 <silvershade1000@gmail.com>
2024-08-21 13:42:54 +0330

    style: improve Testimonials  responsiveness for mobile

--- src/components/TestimonialCard.tsx
+++ src/components/TestimonialCard.tsx
@@ -7,22 +7,23 @@ interface TestimonialCardType {
   img: string;
 }
 
+
 function TestimonialCard({ name, job, comment, img }: TestimonialCardType) {
   return (
-    <div className="max-w-xs sm:max-w-sm md:max-w-md mx-auto p-4 sm:p-6 bg-backgroundGray rounded-xl shadow-xl">
+    <div className="max-w-xs sm:max-w-sm md:max-w-md mx-auto p-4 sm:p-6 bg-backgroundGray rounded-xl shadow-xl flex flex-col items-center">
       <MdFormatQuote size={40} className="text-darkGray my-4" />
-      <p className="text-base sm:text-lg text-darkGray mb-4 sm:mb-6">
+      <p className="text-base sm:text-lg text-darkGray mb-4 sm:mb-6 text-center">
         {comment}
       </p>
       <div className="flex items-center">
-        <div className="w-10 h-10 sm:w-12 sm:h-12">
+        <div className="w-12 h-12 sm:w-14 sm:h-14">
           <img
             src={img}
             alt="Testimonial"
             className="rounded-full w-full h-full object-cover"
           />
         </div>
-        <div className="ml-3 sm:ml-4">
+        <div className="ml-3 sm:ml-4 text-center sm:text-left">
           <p className="text-sm sm:text-base font-semibold text-orangePeel">
             {name}
           </p>
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -34,20 +34,22 @@ const TestimonialsData = [
 function Testimonials() {
   return (
     <div className="bg-softWhite py-10">
-      <div className="mx-4 sm:mx-8 lg:mx-16 max-w-screen-lg">
-        <h2 className="text-lg sm:text-xl lg:text-2xl text-darkGray mb-6 sm:mb-8">
-          See what others are achieving through learning
-        </h2>
-        <div className="flex flex-wrap gap-4 sm:gap-6 lg:gap-8">
-          {TestimonialsData.map((item, index) => (
-            <TestimonialCard
-              key={index}
-              name={item.name}
-              job={item.job}
-              comment={item.comment}
-              img={item.img}
-            />
-          ))}
+      <div className="flex justify-center">
+        <div className="mx-4 sm:mx-8 lg:mx-16 max-w-screen-2xl">
+          <h2 className="text-lg sm:text-xl lg:text-2xl text-darkGray mb-6 sm:mb-8 text-center">
+            See what others are achieving through learning
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-12">
+            {TestimonialsData.map((item, index) => (
+              <TestimonialCard
+                key={index}
+                name={item.name}
+                job={item.job}
+                comment={item.comment}
+                img={item.img}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
