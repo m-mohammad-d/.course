16e5fa36f39c9a9ad51083da01b9256087a4ced3
silvershade1 <silvershade1000@gmail.com>
2024-08-21 13:02:23 +0330

    style: improve CourseRecomanded responsiveness for mobile

--- src/components/CourseRecomanded.tsx
+++ src/components/CourseRecomanded.tsx
@@ -6,30 +6,30 @@ import Spinner from "./Spinner";
 
 const CourseRecommendations = () => {
   const { data, isLoading } = useGetCourses();
-  const recommendedCourses = data?.splice(1, 5);
-
+  const recommendedCourses = data?.slice(1, 6); 
 
   if (isLoading) return <Spinner />;
+
   return (
-    <div className="max-w-7xl mx-auto py-12 h-full">
-      <h2 className="text-2xl font-bold mb-6 text-darkGray">
+    <div className="max-w-7xl px-4 mx-auto py-12">
+      <h2 className="text-3xl font-bold mb-6 text-darkGray">
         What to learn next
       </h2>
 
       <div>
         <div className="flex justify-between items-center mb-8">
-          <h3 className="text-lg font-semibold text-mediumGray">
+          <h3 className="text-xl font-semibold text-mediumGray">
             Recommended for you
           </h3>
           <Link
             to="/courses"
-            className="flex items-center gap-4 text-primary hover:text-darkOrange transition duration-300 ease-in-out"
+            className="flex items-center gap-2 text-primary hover:text-darkOrange transition duration-300 ease-in-out"
           >
-            <p>See all courses</p>
-            <FaArrowRight />
+            <p className="hidden sm:inline">See all courses</p>
+            <FaArrowRight size={20} />
           </Link>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
           {recommendedCourses.map((course, index) => (
             <CourseCards key={index} course={course} />
           ))}
